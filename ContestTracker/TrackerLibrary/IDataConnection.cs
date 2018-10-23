8b113959ba6f6220ee6bbd9d76ae0588ab3e401a
Mateusz Jelen - DELL <mateuszjelen@gmail.com>
2018-10-23 09:34:37 +0300

    Added interface IDataConnection

---
+++ ContestTracker/TrackerLibrary/IDataConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    //We need to specify what items need to be in this "contract"
+    public interface IDataConnection
+    {
+        PrizeModel CreatePrize(PrizeModel model);
+    }
+}
