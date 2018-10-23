c553a4c33b01bc28036384b5c5eceb03f6410847
Mateusz Jelen - DELL <mateuszjelen@gmail.com>
2018-10-23 09:23:55 +0300

    Added static GlobalConfig.cs class - class initialize connections with text files or database

---
+++ ContestTracker/TrackerLibrary/GlobalConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public static class GlobalConfig
+    {
+        public static List<IDataConnection> Connections { get; private set; }//private set - only methods inside of the GlobalConfig class can change the value of Connection but everyone can read the value of Connection
+
+        public static void InitializeConnection(bool dataBase, bool textFiles)
+        {
+            if (true == dataBase)
+            {
+                // TODO - Create the SQL connection
+            }
+
+            if(true == textFiles)
+            {
+                //TODO - Create the Text Connextion
+            }
+        }
+    }
+}
